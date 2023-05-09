28abd13bb75956f53b15c77ba944a9dc2b87bf51
gabrielsantiag0 <gabrielleandrosantiago9@gmail.com.br>
2023-05-09 03:24:56 -0300

    arruma y arruma, Arrumando Receitas

--- ReceitasLanches/menu.php
+++ ReceitasLanches/menu.php
@@ -4,39 +4,31 @@
   <title>Menu Acessa DMellitus</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
+  <link rel="stylesheet" href="../css/estilo.css">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
 </head>
 <body>
-  <style>
-    .row{
-      margin: 0%;
-    }
-  </style>
-<header class="">
-      <nav class="navbar navbar-expand-lg bg-body-tertiary border-bottom border-danger">
-        <div class="container-fluid bg-white">
-          <a class="navbar-brand text-danger mx-4" href="../index.php"><img src="../Img/logopequenaweb.png" alt="" srcset=""> </a>
-          <div class="collapse navbar-collapse navbar-right d-flex justify-content-center hiden" id="navbarNav">
-            <ul class="navbar-nav">
-              <li class="nav-item mx-3">
-                <a class="nav-link active text-danger fs-4" aria-current="page" href="../index.php">Home</a>
-              </li>
-              <li class="nav-item mx-e">
-                <a class="nav-link text-danger fs-4" href="../FaleConosco.php">Contato</a>
-              </li>
-              <li class="nav-item mx-3">
-                <a class="nav-link text-danger fs-4" href="../download.php">Download</a>
-              </li>
-              <li class="nav-item mx-3">
-                <a class="nav-link text-danger fs-4" href="../sobreNos.php">Sobre nós</a>
-              </li>
-              </div>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </nav>
-    </header>
+<nav class="navbar navbar-expand-lg bg-danger">
+  <div class="container-fluid">
+    <a class="navbar-brand text-white" href="../index.php"><img src="Img/logopequenaweb.png" alt=""></a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav ajus">
+        <li class="nav-item">
+          <a class="nav-link active text-white"href="../index.php">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link text-white" href="../receita.php">Receitas</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link text-white" href="#contato">Contato</a>
+        </li>
+      </ul>
+    </div>
+  </div> 
+</nav>
 </body>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
 </html>
